d99c0ca901300dc3f26771c25853bd501fed5a29
duncan@aquaya <duncan@aquaya.org>
2025-03-25 19:27:18 +0000

    Adding config to separate some common fctly

---
+++ wqaf-kenya/config.py
@@ -0,0 +1,33 @@
+"""
+Config module for WQAF Kenya Expansion
+"""
+
+primary_counties = [
+    "Nakuru",
+    "Mombasa",
+    "Kiambu",
+    "Uasin Gishu",
+    "Nyeri"
+]
+
+secondary_counties = [
+    "Kirinyaga",
+    "Embu",
+    "Nyandarua",
+    "Laikipia",
+    "Meru",
+    "Murang'a",
+    "Nandi",
+    "Elgeyo-Marakwet",
+    "Kilifi",
+    "Kwale",
+    "Taita Taveta"
+]
+
+selected_labs = [
+    "Nakuru Water and Sanitation Services Company Limited",
+    "Polucon Services Kenya Limited",
+    "Crop Nutrition Laboratory Services Limited",
+    "Eldoret Water And Sanitation Company Limited",
+    "Nyeri Water and Sanitation Company"
+]
